775d1b9ee8153de3f578989066baa5bc89391685
puneet5ingh <puneet.singh@edu.sait.ca>
2024-05-21 21:11:41 -0600

    Add week 2  and update page.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>PUNEET SINGH</h1>
+      <p>
+        <Link href= "https://github.com/puneet5ingh/cprg306-assignments.git">
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
